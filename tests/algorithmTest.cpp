93c72c45e2baec51afbdbdf39ba476c1b800d47c
alfeim <alfeim@287b3242-7fab-264f-8401-8509467ab285>
2008-01-08 05:33:07 +0000

    git-svn-id: http://southwind:2481/svn/cpp_lib/trunk@184 287b3242-7fab-264f-8401-8509467ab285

---
+++ tests/algorithmTest.cpp
@@ -0,0 +1,31 @@
+#include <cppunit/extensions/HelperMacros.h>
+#include <util/algorithm.hpp>
+
+class Sum
+{
+public:
+	int operator()(int lhs, int rhs) const
+	{
+		return lhs + rhs;
+	}
+};
+
+class AlgorithmTest : public CppUnit::TestFixture
+{
+	CPPUNIT_TEST_SUITE(AlgorithmTest);
+	CPPUNIT_TEST(reduceTest);
+	CPPUNIT_TEST_SUITE_END();
+public:
+	void reduceTest()
+	{
+		std::vector<int> vec(10);
+		for (int offset = 1; offset <= 10; ++offset)
+			vec[offset-1] = offset;
+
+		CPPUNIT_ASSERT(
+ 			reduce<std::vector<int>::const_iterator, Sum, int>
+			(vec.begin(), vec.end(), Sum()) == 55);
+	}
+};
+
+CPPUNIT_TEST_SUITE_REGISTRATION( AlgorithmTest );
